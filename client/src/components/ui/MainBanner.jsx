cb114932a78b6c1f1bf602c9ae6ad0fc4c7716a6
Rajashekharvn <raju.naduvinahalli@gmail.com>
2026-01-09 13:47:24 +0530

    style: remove arrow icons from main banner

--- client/src/components/ui/MainBanner.jsx
+++ client/src/components/ui/MainBanner.jsx
@@ -25,22 +25,12 @@ const MainBanner = () => {
             className="group flex items-center gap-2 px-7 md:px-9 py-3 bg-primary hover:bg-primary-dull transition rounded text-white cursor-pointer"
           >
             Shop now
-            <img
-              className="md:hidden transition group-focus:translate-x-1"
-              src={assets.white_arrow_icon}
-              alt="arrow"
-            />
           </Link>
           <Link
             to="/products"
             className="group hidden md:flex items-center gap-2 px-9 py-3 cursor-pointer"
           >
             Explore Deal
-            <img
-              className="transition group-focus:translate-x-1"
-              src={assets.black_arrow_icon}
-              alt="arrow"
-            />
           </Link>
         </div>
       </div>
