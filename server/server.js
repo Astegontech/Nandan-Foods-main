d2ede92377ab29f5cc53f9d4469772f0bbbb4eb2
Rajashekhar Naduvinahalli <103878269+Rajashekharvn@users.noreply.github.com>
2026-01-12 01:55:04 +0530

    Remove duplicate allowedOrigins in CORS settings

--- server/server.js
+++ server/server.js
@@ -17,11 +17,7 @@ await connectDB();
 await connectCloudinary();
 
 const allowedOrigins = [
-  "http://localhost:5173",
-  "http://localhost:5174",
-  "http://localhost:4000",
-  "http://localhost:3000",
-  "http://nandanfoods.store",
+  "http://nandanfoods.store"
 ];
 
 const corsOptions = {
